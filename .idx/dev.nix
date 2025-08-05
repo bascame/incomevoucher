fdf3c598fde6b1cc673c031efe5d96465a9b0417
bascamewifinet <bascamewifinet@gmail.com>
2025-08-05 14:56:55 +0000

    main

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,3 +1,16 @@
+# dev.nix
+
+    # Mengonfigurasi pratinjau web untuk aplikasi Anda.
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          # Perintah ini memulai server web yang menyajikan file dari direktori .idx Anda.
+          command = ["npx", "http-server", ".idx", "-p", "$PORT", "--cors"];
+          manager = "web";
+        };
+      };
+    };
 { pkgs, ... }: {
   # Menentukan channel nixpkgs. 'stable-24.05' adalah pilihan yang baik untuk stabilitas.
   channel = "stable-24.05";
